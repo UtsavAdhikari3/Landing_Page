11a19c0080d42c68480f2add060ecb44dab35c2a
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-08-26 13:19:34 +0545

    ex-2 complete

--- script.js
+++ script.js
@@ -5,12 +5,14 @@
  * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
  */
 
-number = Number(prompt("enter a number"));
+number = prompt("enter a number");
 
 function numberChecker() {
-	if(number === 6) {
+	if(number >= 10) {
 		return true;
 	} else {
 		return false;
 	}
 }
+
+console.log(numberChecker())
