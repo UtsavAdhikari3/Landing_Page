854fbafa97b1c7d5ae4458ffbcdadf7526d1dd4a
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-08-26 13:22:03 +0545

    ex-3 complete

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 /**
- * The code below tells the browser to ask you for a number
- * then if that number is `6`, it returns `true` otherwise it returns `false`
- * 
- * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ * Lets do some math!
+ * Some rules first:
+ *   - Replace the strings to the right of the = with the math expression they describe.
+ *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
  */
 
-number = prompt("enter a number");
+const a = 1 + 8;
+const b = 22 * 3;
+const c = 5 % 4;
+const d = a - 17;
+const e = a + b + c + d;
 
-function numberChecker() {
-	if(number >= 10) {
-		return true;
-	} else {
-		return false;
-	}
-}
+console.log(e);
 
-console.log(numberChecker())
