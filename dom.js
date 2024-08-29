b7083a2a297fb078b03a10a7d550658604f5a169
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-08-29 20:11:32 +0545

    Exercise completed by adding a div and additional element to that div

--- dom.js
+++ dom.js
@@ -15,6 +15,21 @@ headingThree.classList.add("color-blue");
 headingThree.textContent = "I'm blue h3";
 headingThree.style.cssText = "color:blue;"
 
+const newDiv = document.createElement("div");
+newDiv.classList.add("my-div");
+newDiv.style.border = "1px solid black";
+newDiv.style.backgroundColor = "pink";
+
+const headingOne = document.createElement("h1");
+headingOne.textContent = "I'm in a div."
+const para2 = document.createElement("p");
+para2.textContent = "ME TOO!"
+
+
 container.appendChild(content);
 container.appendChild(para);
 container.appendChild(headingThree);
+container.appendChild(newDiv);
+newDiv.appendChild(headingOne);
+newDiv.appendChild(para2)
+
